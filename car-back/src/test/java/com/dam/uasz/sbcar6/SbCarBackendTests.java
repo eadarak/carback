7f8e7b1f23e825123310b6d6eeb904d6102483bd
El Abdou Drame <eha.d1@zig.univ.sn>
2023-10-26 21:20:11 +0000

    the backend

---
+++ car-back/src/test/java/com/dam/uasz/sbcar6/SbCarBackendTests.java
@@ -0,0 +1,13 @@
+package com.dam.uasz.sbcar6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCarBackendTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
